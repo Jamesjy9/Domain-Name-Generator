d1d555f4282aa9e18daacd95e4a19b89ec164b39
Jamesjy9 <jamesnore1989@gmail.com>
2021-10-31 00:33:36 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["my", "our"];
+  let adj = ["little", "grand"];
+  let noun = ["father", "girl"];
+  let extend = [".com", ".cl", ".ht", ".ca"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < extend.length; m++) {
+          console.log(
+            pronoun[i]
+              .concat(adj[j])
+              .concat(noun[k])
+              .concat(extend[m])
+          );
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
